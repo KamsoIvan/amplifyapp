50ff3ee24922bab8a17cdf2b8d9e92f3509a9102
botrel <tchonangbotrel@gmail.com>
2022-07-24 23:34:49 +0100

    added auth

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+// import '@aws-amplify/ui-react/styles.css'; 
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header>
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>We have now Auth!</h1>
+//         <button>rrrrrr</button>
+//       </header>
+//       {/* <AmplifySignOut /> */}
+//     </div>
+//   );
+// }
+
+// export default withAuthenticator(App);
+
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.svg'
 import './App.css';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { a } from 'aws-amplify';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-        <h2>Hello from V3</h2>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <p>
+            Hey {user.username}, welcome to my channel, with auth!
+          </p>
+          <button onClick={signOut}>Sign out</button>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
 export default App;
+
+
+
+// <div className="App">
+    //   <header>
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <h1>We now have Auth!</h1>
+    //   </header>
+    // </div>
