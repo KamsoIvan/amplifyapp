5aebf455680b0927ac8806ad0bfe647ebc3cafa6
botrel <tchonangbotrel@gmail.com>
2022-07-25 00:02:30 +0100

    added auth

--- src/App.js
+++ src/App.js
@@ -24,17 +24,15 @@ import React from 'react';
 import logo from './logo.svg'
 import './App.css';
 
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import { a } from 'aws-amplify';
-
 function App() {
   return (
     <Authenticator>
       {({ signOut, user }) => (
         <div className="App">
           <p>
-            Hey {user.username}, welcome to my channel, with auth!
+            Hey {user.username}, welcome to app, with auth!
           </p>
           <button onClick={signOut}>Sign out</button>
           <img src={logo} className="App-logo" alt="logo" />
@@ -44,13 +42,4 @@ function App() {
   );
 }
 
-export default App;
-
-
-
-// <div className="App">
-    //   <header>
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <h1>We now have Auth!</h1>
-    //   </header>
-    // </div>
+export default withAuthenticator(App);
